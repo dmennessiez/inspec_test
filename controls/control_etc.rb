c4e7e2cfbcadc85e237159a167a8e3933ca9e052
dmennessiez <109960989+dmennessiez@users.noreply.github.com>
2022-07-28 09:12:03 -0700

    Update control_etc.rb

--- controls/control_etc.rb
+++ controls/control_etc.rb
@@ -1,1 +1,10 @@
+title 'Installation validation'
 
+control 'windows-Firefox' do
+  title 'Ensure \'Mozilla Firefox\' is \'Installed\''
+  desc 'It is a part of the Golden AMI provisioning'
+  impact 1.0
+  describe registry_key('HKEY_LOCAL_MACHINE\\SOFTWARE\\Mozilla\\Mozilla Firefox') do
+    it { should exist }
+  end
+end
